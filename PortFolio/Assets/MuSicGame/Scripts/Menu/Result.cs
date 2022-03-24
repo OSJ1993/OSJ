371b81696a726c48aa5bf7bc89156ef697c1e796
OSJ1993 <90177830+OSJ1993@users.noreply.github.com>
2022-03-24 19:18:02 +0900

    220324
    [유니티 강좌] 리듬 게임 튜토리얼 12 - 낭떠러지 추락

--- PortFolio/Assets/MuSicGame/Scripts/Menu/Result.cs
+++ PortFolio/Assets/MuSicGame/Scripts/Menu/Result.cs
@@ -16,6 +16,7 @@ public class Result : MonoBehaviour
     ComboManager theCombo;
     TimingManager theTiming;
 
+    //
     void Start()
     {
         theScore = FindObjectOfType<ScoreManager>();
