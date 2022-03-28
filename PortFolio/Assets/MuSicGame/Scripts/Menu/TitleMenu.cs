361350531dcc910d1c77bc4c3eb8751fa5712d5c
OSJ1993 <90177830+OSJ1993@users.noreply.github.com>
2022-03-28 15:22:06 +0900

    22.03.28
    [유니티 강좌] 리듬 게임 튜토리얼 15 - 메뉴 구현 4:46초

---
+++ PortFolio/Assets/MuSicGame/Scripts/Menu/TitleMenu.cs
@@ -0,0 +1,13 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TitleMenu : MonoBehaviour
+{
+    [SerializeField] GameObject goStageUI = null;
+    
+    public void BtnPlay()
+    {
+
+    }
+}
