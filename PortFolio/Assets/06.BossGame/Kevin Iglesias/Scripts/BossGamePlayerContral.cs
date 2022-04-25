101e1739988a62ec480bf0021f734c6c82d17df6
OSJ1993 <90177830+OSJ1993@users.noreply.github.com>
2022-04-25 22:21:31 +0900

    22.04.25

--- PortFolio/Assets/06.BossGame/Kevin Iglesias/Scripts/BossGamePlayerContral.cs
+++ PortFolio/Assets/06.BossGame/Kevin Iglesias/Scripts/BossGamePlayerContral.cs
@@ -19,12 +19,14 @@ public class BossGamePlayerContral : MonoBehaviour
 
     void PlayerMove()
     {
+        
         //keyboard 방향키에 따라 이동 방향 정해지는 기능 22.04.22 by승주
         movement = new Vector3(Input.GetAxis("Horizontal"), 0,
                                                        Input.GetAxis("Vertical"));
 
         if (movement != Vector3.zero)
         {
+            
             //player의 움직임이 있다면 camera도 같이 움직이게 하는 기능 22.04.22 by승주
             playerAxis.rotation = Quaternion.Euler(new Vector3(0, bossGameCameraContral.camAxis_contral.rotation.y
                                                                                                          + bossGameCameraContral.mouseX, 0)
@@ -37,7 +39,11 @@ public class BossGamePlayerContral : MonoBehaviour
                                                  , Quaternion.LookRotation(movement), 5 * Time.deltaTime);
         }
 
-        bossGameCameraContral.camAxis_contral.position = player.position;
+        //camera가 player의 가슴 축 쯤을 따라가게 하는 기능 22.04.23 by승주
+        bossGameCameraContral.camAxis_contral.position = new Vector3(player.position.x
+                                                                                                                            ,player.position.y+0.5f
+                                                                                                                            ,player.position.z);
+        
     }
 
 
