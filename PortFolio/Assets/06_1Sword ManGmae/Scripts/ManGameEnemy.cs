d8e7e1a1d1ebe72020a54cdc835ee44ee487035f
OSJ1993 <90177830+OSJ1993@users.noreply.github.com>
2022-04-29 16:55:57 +0900

    22.04.29
    22:14초까지 봄

--- PortFolio/Assets/06_1Sword ManGmae/Scripts/ManGameEnemy.cs
+++ PortFolio/Assets/06_1Sword ManGmae/Scripts/ManGameEnemy.cs
@@ -23,9 +23,21 @@ public class ManGameEnemy : MonoBehaviour
     //player에게 맞을 때 기능 22.04.29 승주
     private void OnTriggerEnter(Collider other)
     {
-        if(other.gameObject.tag== "SwordManPlayerAtk")
+        if (other.gameObject.tag == "SwordManPlayerAtk")
         {
+            //HitEffect 기능 22.04.29 승주
 
+            //hitPosition는 공격한 Object의 위치와 맞은 Object의 위치를 + 하고 0.5f *에서 반으로 나눈 player와 enemy의 위치가 되는 기능 22.04.29 승주
+            Vector3 hitPosition = (other.transform.position + transform.position) * 0.5f;
+
+            //Damage는 공격한 부모Object의 player의 공격력을 가져와서 Damage로 만든 기능 22.04.29 승주
+            //Damge를 Random하게 주기 위한 기능 22.04.29 승주
+            int dmg = (int)(other.GetComponentInParent<ManGamePlayer>().atk * Random.Range(0.8f, 2f));
+
+            ManGameHitManager.instance.DmgTextOn(hitPosition, dmg);
+
+            //Damage만큼 HP가 깎이게 하는 기능 22.04.29 승주
+            curHp -= dmg;
         }
     }
 }
--- PortFolio/Assets/06_1Sword ManGmae/Scripts/ManGameHitManager.cs
+++ PortFolio/Assets/06_1Sword ManGmae/Scripts/ManGameHitManager.cs
@@ -6,6 +6,43 @@ using UnityEngine;
 
 public class ManGameHitManager : MonoBehaviour
 {
+    public static ManGameHitManager instance;
+
     public TextMeshProUGUI[] dmgText;
     public Camera cam;
+
+    Vector3 upPosition = new Vector3(0, 2, 0);
+
+    private void Awake()
+    {
+        //싱글톤 패턴 기능 22.04.29 승주
+        if (instance == null) instance = this;
+    }
+
+    //damageText들을 띄우는 기능 22.04.29 승주
+    public void DmgTextOn(Vector3 hitPosition,int dmg)
+    {
+        for (int i = 0; i < dmgText.Length; i++)
+        {
+            //i번째 damageText가 켜져있지 않다면 이미 켜져 있는 text는 pass하고 그 다음 안 켜져있는 damgeText를 켜기 위한 기능 22.04.29 승주
+            if (!dmgText[i].gameObject.activeSelf)
+            {
+                //안 켜져 있는 Text의 위치는 hitPosition 이다 기능 22.04.29 승주
+                //Camera.WorldToScreenPotin(A) => 월드 좌표르 A를 화면 좌표로 변경
+                dmgText[i].transform.position = cam.WorldToScreenPoint(hitPosition+ upPosition);
+
+                //안 켜져있던 damageText의 Text의 내용은 얼마인지는 모르지만 Damage다 라는 기능 22.04.29 승주
+                dmgText[i].text = dmg.ToString();
+                dmgText[i].gameObject.SetActive(true);
+
+                //다이나믹한 효과를 위해서 Damage에 따라서 fontSize 변화 시키는 기능 22.04.29 승주
+                dmgText[i].fontSize = dmg * 0.5f;
+
+                //text하나가 켜지면 다른 text가 켜지지 않게 하는 기능 22.04.29
+                return;
+                
+
+            }
+        }
+    }
 }
